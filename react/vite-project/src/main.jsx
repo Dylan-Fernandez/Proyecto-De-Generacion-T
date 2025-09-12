985cf58019270d301d0c35a864b4f7681e71881a
Dylan Fernández <fdylan2006@gmail.com>
2025-09-12 15:53:06 -0300

    Se creo los componentes Header, Footer y se cambio el estilo de la pagina

--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Information from './pages/Information.jsx'
 import './index.css'
 import App from './App.jsx'
+import Header from './componetes/Header.jsx'
+import Footer from './componetes/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header></Header>
     <App />
-  </StrictMode>,
+    <Information />
+    <Footer/>
+  </StrictMode>
 )
---
+++ react/vite-project/src/pages/Information.jsx
@@ -0,0 +1,15 @@
+function Information() {
+return (
+    <>
+    <h1>¿Que es IDM?</h1>
+    <p>
+        Nuestro trabajo se centra en la necesidad de abordar los desafíos de comunicación y 
+        aprendizaje de estudiantes con discapacidad motriz, específicamente aquellos con 
+        limitaciones significativas de movimiento y habla.Para resolver los problemas que 
+        tienen diseñamos un programa de uso simple y practico para las personas que tengan 
+        esta problematica.
+    </p>
+    </>
+);
+}
+export default Information;
