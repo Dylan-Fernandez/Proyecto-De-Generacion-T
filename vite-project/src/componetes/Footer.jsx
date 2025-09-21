0428e56a9cc5afd2fe22bf59ba7c9ae4325d6377
FERCHOXXD69 <ffeerr.gabriel@gmail.com>
2025-09-21 17:51:26 -0300

    Add files via upload
    footer actualizado

--- vite-project/src/componetes/Footer.jsx
+++ vite-project/src/componetes/Footer.jsx
@@ -1,12 +1,28 @@
+import "./Footer.css";
+
 function Footer() {
   return (
     <>
-    <div style={{ backgroundColor: '#f0f0f0', padding: '20px', textAlign: 'center', marginTop: "50px" }}>
-      <p style={{ fontSize: '0.875rem', color: '#666' }}>
-        &copy; {new Date().getFullYear()} My Company. All rights reserved.
-      </p>
+    <footer className="footer">
+        <div class="participantes">
+            <h3>Participantes:</h3>
+            <ul>
+                <li><a href="https://www.instagram.com/dylan_youl/profilecard/?igsh=MXM4aDdjOHZkaW16Nw=="target="_blank">Dylan Fernández </a> </li>
+                <li><a href="https://es.wikipedia.org/wiki/Dios" target="_blank">Deymar Ovidio</a> </li>
+                <li><a href="https://www.youtube.com/watch?v=qU9mHegkTc4"target="_blank">Fernando Tapia</a> </li>
+            </ul>
+        </div>
+        <div className="social">
+          <h3>nuestras redes sociales</h3>
+          <a href="https://www.instagram.com/idm__oficial?igsh=ZGlndjFyb3dtZXVp" target="_blank">Instagram</a>
+          <a href="https://www.facebook.com/share/1CmiBztcT9/" target="_blank">Facebook</a>
+          <a href="https://x.com/IDM__Oficial?t=ONRRnjcy5gozZGK6y0ZfzA&s=33" target="_blank">Twitter</a>
+
+        &copy; {new Date().getFullYear()}   I.D.M. All rights reserved.
       </div>
+    </footer>
     </>
   );
 }
-export default Footer;
+
+export default Footer; 
