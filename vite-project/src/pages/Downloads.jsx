d7d0d81cf3ea280e0d2af016b769e28988329d72
Dylan-Fernandez <fdylan2006@gmail.com>
2025-09-22 23:55:30 -0300

    Se actualizo el home y se creo su css,
    al igual que el download

--- vite-project/src/pages/Downloads.jsx
+++ vite-project/src/pages/Downloads.jsx
@@ -1,12 +1,35 @@
-// src/pages/Contacts.jsx
+import "./Downloads.css";
 
 const Downloads= () => {
   return (
-    <div>
-      <h1>descargar</h1>
-
-    </div>
+    <>
+<section class="dowloads" >
+        <h3 class="dowloads_title">Descargas</h3>
+        <div class="dowloads_links">
+            <div class="dowloads_link">
+                <h4>I.D.M Version 1</h4>
+                <a href="https://mega.nz/file/i3BQBKoA#WWFJGE19FUMc2nTz-Q0Z3vANByCIjGI9HM93E-UMvCU" target="_blank">https://mega.nz/file/I.D.M_V1</a>
+            </div>
+            <div class="dowloads_link">
+                <h4>I.D.M Version 2</h4>
+                <a href="https://mega.nz/file/aiozFZiB#ZdPeUlc-CwjzpSnQu1_9ROpSecrcmR2OPaMQclgGMlc" target="_blank">https://mega.nz/file/I.D.M_V2</a>
+            </div>
+            <div class="dowloads_link">
+                <h4>I.D.M Version 3</h4>
+                <a href="https://mega.nz/file/f7Q0WbSQ#sVzNH-c-djKC58fRe_wTZFcgxwYAcHIvgVG0aNrM6Hk" target="_blank">https://mega.nz/file/I.D.M_V3</a>
+            </div>
+            <div class="dowloads_link">
+                <h4>I.D.M Version 4</h4>
+                <a href="https://mega.nz/file/yyomyTrT#BUAaD0JKmbj_mnD9q6ozZyxsNC6fybju82uiD_T3EUk" target="_blank">https://mega.nz/file/I.D.M_V4</a>
+            </div>
+            <div class="dowloads_link">
+                <h4>I.D.M Version 5</h4>
+                <a href="https://mega.nz/file/unIXmCxT#kAamMr-AaPRM6OK5F22iQH65XWWyRBMxnHdoOfh9FjA" target="_blank">https://mega.nz/file/I.D.M_V5</a>
+            </div>
+        </div>
+    </section>
+    </>
   );
 };
 
-export default Downloads;  // <-- Esta línea es clave 
+export default Downloads;  // <-- Esta línea es clave
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,15 +1,23 @@
+import que_esImg from "./img/que_es.png"; 
+import "./Home.css";
 function Home() {
 return (
     <>
-    <h1>¿Que es IDM?</h1>
-    <p>
-        Nuestro trabajo se centra en la necesidad de abordar los desafíos de comunicación y 
-        aprendizaje de estudiantes con discapacidad motriz, específicamente aquellos con 
-        limitaciones significativas de movimiento y habla. Para resolver los problemas que 
-        tienen diseñamos un programa de uso simple y practico para las personas que tengan 
-        esta problematica.
-    </p>
+    <div className="chart_text">
+        <img className="img_what_is" src={que_esImg} alt="cara de pregunta" />
+        <div className="right_what_is">
+        <h2 className="title_what_is">¿QUE ES I.D.M?</h2>
+        <p className="what_is_text">
+            Nuestro trabajo se centra en la necesidad de abordar los desafíos de comunicación y
+            aprendizaje de estudiantes con discapacidad motriz, específicamente aquellos con limitaciones 
+            significativas de movimiento y habla. Para resolver los problemas que tienen diseñamos un programa
+            de uso simple y practico para las personas que tengan esta problematica.
+        </p>
+        <div id="Historia"></div>
+        </div>
+    </div>
     </>
 );
 }
+
 export default Home;
