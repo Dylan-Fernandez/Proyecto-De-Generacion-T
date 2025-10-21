e5bc2b255208b5030cdfe344c7b9b864ffde3043
fernando <ffeerr.gabriel@gmail.com>
2025-10-21 19:56:11 -0300

    cambios en descargas y home

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,23 +1,33 @@
-import que_esImg from "./img/que_es.png"; 
+import que_esImg from "./img/que_es.png";
 import "./Home.css";
-function Home() {
-return (
+
+const Home = () => {
+  return (
     <>
-    <div className="chart_text">
-        <img className="img_what_is" src={que_esImg} alt="cara de pregunta" />
+    <section className="home" id="home">
+      <div className="chart_text">
+        <img
+          className="img_what_is"
+          src={que_esImg}
+          alt="ilustración sobre inclusión motriz"
+        />
         <div className="right_what_is">
-        <h2 className="title_what_is">¿QUE ES I.D.M?</h2>
-        <p className="what_is_text">
-            Nuestro trabajo se centra en la necesidad de abordar los desafíos de comunicación y
-            aprendizaje de estudiantes con discapacidad motriz, específicamente aquellos con limitaciones 
-            significativas de movimiento y habla. Para resolver los problemas que tienen diseñamos un programa
-            de uso simple y practico para las personas que tengan esta problematica.
-        </p>
-        <div id="Historia"></div>
+          <h2 className="title_what_is">¿Qué es I.D.M?</h2>
+          <p className="what_is_text">
+            Nuestro trabajo se centra en la necesidad de abordar los desafíos de
+            comunicación y aprendizaje de estudiantes con discapacidad motriz,
+            específicamente aquellos con limitaciones significativas de
+            movimiento y habla. <br />
+            <br />
+            Para resolver los problemas que tienen, diseñamos un programa de uso
+            simple y práctico pensado para las personas que enfrentan esta
+            problemática.
+          </p>
         </div>
-    </div>
-    </>
-);
-}
+      </div>
+    </section>
+</>
+  );
+};
 
 export default Home;
